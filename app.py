c4c3f7b78128d3f887917c2f1e00d1221ea38489
PieNaa <81947057+PieNaa@users.noreply.github.com>
2023-07-18 10:24:32 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import math
+from PIL import Image
+from streamlit_image_coordinates import streamlit_image_coordinates
+
+def get_data():
+    if 'df' not in st.session_state:
+        data = {
+            'Point': ['Lightsource_Top_Point', 'Lightsource_Buttom_Point', 'Spectrum_Line_Mid_Point'],
+            'X-Coordinate': [0, 0, 0],
+            'Y-Coordinate': [0, 0, 0],
+        }
+        st.session_state.df = pd.DataFrame(data)
+    return st.session_state.df
+
+def main():
+    df = get_data()
+
+    st.title("Spectrometer สำหรับ พาย ปันปัน มิว แทน เท่านั้นนน ใช้กลุ่มอื่นค่าจะผิดเด้อ")
+
+    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+    if uploaded_image is not None:
+        image = Image.open(uploaded_image)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+
+        st.write("ค่อย ๆ กด จิ้มเลือกพิกัดจ้า:")
+
+        clicked_coor = streamlit_image_coordinates(
+            image,
+            height=500,
+            key="pil",
+        )
+
+        st.write(clicked_coor)
+
+        st.write("---*---*---*---*---*---*---*---*---*---*---")
+
+        if st.button("Submit : Coordinate of the top of the light source"):
+            if clicked_coor:
+                df.loc[df['Point'] == 'Lightsource_Top_Point', 'X-Coordinate'] = clicked_coor.get("x")
+                df.loc[df['Point'] == 'Lightsource_Top_Point', 'Y-Coordinate'] = clicked_coor.get("y")
+                st.write(f"You submitted: {clicked_coor}")
+
+        st.write("---*---*---*---*---*---*---*---*---*---*---")
+
+        if st.button("Submit : Coordinate of the buttom of the light source"):
+            if clicked_coor:
+                df.loc[df['Point'] == 'Lightsource_Buttom_Point', 'X-Coordinate'] = clicked_coor.get("x")
+                df.loc[df['Point'] == 'Lightsource_Buttom_Point', 'Y-Coordinate'] = clicked_coor.get("y")
+                st.write(f"You submitted: {clicked_coor}")
+
+        st.write("---*---*---*---*---*---*---*---*---*---*---")
+        
+        if st.button("Submit : Coordinate of the midpoint of the spectrum line"):
+            if clicked_coor:
+                df.loc[df['Point'] == 'Spectrum_Line_Mid_Point', 'X-Coordinate'] = clicked_coor.get("x")
+                df.loc[df['Point'] == 'Spectrum_Line_Mid_Point', 'Y-Coordinate'] = clicked_coor.get("y")
+                st.write(f"You submitted: {clicked_coor}")
+
+        st.write(df)
+
+        st.write("---*---*---*---*---*---*---*---*---*---*---")
+        
+        if st.button("Calculate the wavelength of the selected spectrum line"):
+            if clicked_coor:
+                length_of_light_source = math.sqrt((df.loc[0, 'X-Coordinate']-df.loc[1, 'X-Coordinate'])**2 + (df.loc[0, 'Y-Coordinate']-df.loc[1, 'Y-Coordinate'])**2)
+                midpoint_of_light_source_x = (df.loc[0, 'X-Coordinate'] + df.loc[1, 'X-Coordinate'])/2
+                midpoint_of_light_source_y = (df.loc[0, 'Y-Coordinate'] + df.loc[1, 'Y-Coordinate'])/2
+                distance_to_spectrum = math.sqrt((df.loc[2, 'X-Coordinate'] - midpoint_of_light_source_x)**2 + (df.loc[2, 'Y-Coordinate'] - midpoint_of_light_source_y)**2)
+                constant_val = 1/(10000*11)
+                y_val = 2*distance_to_spectrum/length_of_light_source
+                lambda_val = int(y_val*constant_val*10000000)
+                st.write(f"Wavelength: {lambda_val} nm")
+
+
+
+
+if __name__ == "__main__":
+    main()
